9df32052cab964d5d2d1f22088adc51307dade35
Ruslan <fel@mail.ru>
2020-05-12 14:02:34 +0300

    start JS

---
+++ js/script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+//alert('Hello') 
+
+/* const result = confirm('Вам есть 18 лет?');
+console.log(result); */
+
+/* const answer = prompt('Вам есть 18 лет?', '18');
+console.log(answer); */
+
+/* const answers = [];
+
+ answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваш фамилия?', '');
+answers[3] = prompt('Сколько вам лет?', ''); 
+
+console.log(typeof(answers)); */
+
+
+/* const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+
+const user = "Ruslan"
+
+alert(`Привет, ${user}`)
+ */
+
+
+let incr = 10,
+    decr = 10;
+
+//++incr;
+//--decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2)
+
+console.log(2*4 === 8);
+
+const isChecked = true,
+      isClose = true;
+
+console.log(isChecked && isClose);
