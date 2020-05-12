2f203dedb1628e006ac318af4b3b17e1f90d5ba4
Ruslan <fel@mail.ru>
2020-05-12 15:01:27 +0300

    first task

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ alert(`Привет, ${user}`)
  */
 
 
-let incr = 10,
+/* let incr = 10,
     decr = 10;
 
 //++incr;
@@ -44,4 +44,25 @@ console.log(2*4 === 8);
 const isChecked = true,
       isClose = true;
 
-console.log(isChecked && isClose);
+console.log(isChecked && isClose); */
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?',''); 
+
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;
+
+console.log(personalMovieDB);
+
